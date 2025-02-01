d3ff7f10d70edb48aee26d30e9aaa66ffe02c7e5
Shresth Dwivedi <shresth0109@gmail.com>
2025-02-01 11:01:53 +0530

    complete getCurrentUser action

--- components/navbar/UserMenu.tsx
+++ components/navbar/UserMenu.tsx
@@ -6,8 +6,16 @@ import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
 
-const UserMenu = () => {
+interface UserMenuProps {
+    currentUser?: User | null,
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({
+    currentUser,
+}) => {
 
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
@@ -38,14 +46,46 @@ const UserMenu = () => {
             {isOpen && (
                 <div className="absolute p-4 text-sm right-0 top-14 rounded-xl shadow-md w-[40vw] overflow-hidden md:w-3/4 bg-white">
                     <div className="flex flex-col cursor-pointer">
-                        <MenuItem 
-                            onClick={registerModal.onOpen}
-                            label="Register"
-                        />
-                        <MenuItem 
-                            onClick={loginModal.onOpen}
-                            label="Sign In"
-                        />
+                        {currentUser ? (
+                            <>
+                                <MenuItem 
+                                    onClick={() => {}} 
+                                    label="My trips"
+                                />
+                                <MenuItem 
+                                    onClick={() => {}}
+                                    label="My favorites"
+                                />
+                                <MenuItem 
+                                    onClick={() => {}}
+                                    label="My reservations"
+                                />
+                                <MenuItem 
+                                    onClick={() => {}}
+                                    label="My properties"
+                                />
+                                <MenuItem 
+                                    onClick={() => {}}
+                                    label="Rent my Place"
+                                />
+                                <hr />
+                                <MenuItem 
+                                    onClick={signOut}
+                                    label="Logout"
+                                />
+                            </>                   
+                        ) : (
+                            <>
+                                <MenuItem 
+                                    onClick={registerModal.onOpen}
+                                    label="Register"
+                                />
+                                <MenuItem 
+                                    onClick={loginModal.onOpen}
+                                    label="Sign In"
+                                />
+                            </>
+                        )}
                     </div>
                 </div>
             )}
